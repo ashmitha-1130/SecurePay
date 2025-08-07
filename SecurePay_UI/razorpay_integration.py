81753a69ab1998bd989b38fa81a5bd4eeeb4f767
Ashmitha Sunil <89729447+ashmitha-1130@users.noreply.github.com>
2025-08-07 17:12:41 +0530

    Update razorpay_integration.py

--- SecurePay_UI/razorpay_integration.py
+++ SecurePay_UI/razorpay_integration.py
@@ -2,7 +2,7 @@
 import razorpay
 
 def create_order(amount):
-    client = razorpay.Client(auth=(""YOUR_API_ID", "YOUR_API_KEY"))
+    client = razorpay.Client(auth=("rzp_test_SQeAHlHYsiQuSk", "DO22Ljdt0PCYOJ34UNrPHV1H"))
 
     data = {
         "amount": amount,  # Amount in paise (e.g., 1999 = ₹19.99)
